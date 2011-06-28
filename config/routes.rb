90e4c41e41bb2152bc14e576dc6e0e5aa41913ae
Surendrans <surendrans86@gmail.com>
2011-06-29 01:26:38 +0530

    api key generation done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ApiAuthentication::Application.routes.draw do
+  devise_for :users
+  constraints :subdomain => "api" do
+    resources :projects
+  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +53,9 @@ ApiAuthentication::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  match "/user/gen_apitoken" => "projects#gen_user_apitoken", :as => :gen_api_token
+  match "/user/get_user_apitoken" => "projects#get_user_apitoken"
+  root :to => "projects#index"
 
   # See how all your routes lay out with "rake routes"
 
